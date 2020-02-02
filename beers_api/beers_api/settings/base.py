c3c4359fdd419f4db18c4efcd7cc62ecc90791b7
Nihilistik <jv.nihilistik@gmail.com>
2020-02-02 20:34:12 +0100

    Restored PostgreSQL in django config

--- beers_api/beers_api/settings/base.py
+++ beers_api/beers_api/settings/base.py
@@ -94,17 +94,13 @@ WSGI_APPLICATION = 'beers_api.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     "NAME": get_env_variable("DB_NAME"),
-    #     "USER": get_env_variable("DB_USER"),
-    #     "PASSWORD": get_env_variable("DB_PASS"),
-    #     "HOST": get_env_variable("DB_HOST"),
-    #     "PORT": get_env_variable("DB_PORT"),
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        "NAME": get_env_variable("DB_NAME"),
+        "USER": get_env_variable("DB_USER"),
+        "PASSWORD": get_env_variable("DB_PASS"),
+        "HOST": get_env_variable("DB_HOST"),
+        "PORT": get_env_variable("DB_PORT"),
     }
 }
 
--- infrastructure/local/provision/postgres/init/dbs.sql
+++ infrastructure/local/provision/postgres/init/dbs.sql
@@ -1,2 +1,11 @@
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
-GRANT ALL PRIVILEGES ON *.* TO 'dbuser'@'%';
+create schema beers_db;
+REVOKE ALL ON DATABASE beers_db FROM public;  -- shut out the general public
+GRANT CONNECT ON DATABASE beers_db TO dbuser;  -- since we revoked from public
+
+GRANT USAGE ON SCHEMA public TO dbuser;
+
+GRANT ALL ON ALL TABLES IN SCHEMA public TO dbuser;
+GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO dbuser; -- don't forget those
+
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA beers_db TO dbuser;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA beers_db TO dbuser;
