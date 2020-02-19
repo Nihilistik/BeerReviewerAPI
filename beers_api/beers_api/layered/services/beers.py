43dad7942cde01959674bccc91cb9e1fd7697d37
Jv <jv.nihilistik@gmail.com>
2020-02-19 21:25:31 +0100

    Fixed beer tests

--- beers_api/beers_api/layered/services/beers.py
+++ beers_api/beers_api/layered/services/beers.py
@@ -7,6 +7,12 @@ class BeerService:
         pass
 
     def add_beer(self, beer: dict):
-        new_beer = Beer(beer)
+        new_beer = Beer(**beer)
         new_beer.save()
         return new_beer.id is not None
+
+    def find_beer_by_id(self, id: int):
+        try:
+            return Beer.objects.filter(id=id)[0]
+        except IndexError as e:
+            raise(e)
--- beers_api/beers_api/layered/tests/beers_service_test.py
+++ beers_api/beers_api/layered/tests/beers_service_test.py
@@ -1,16 +1,27 @@
+import pytest
 from django.test import TestCase
 from beers_api.beer_reviews.models.beers import Beer
 from beers_api.beer_reviews.test.factories.fake_beer_factory import BeerFactory
 from beers_api.layered.services.beers import BeerService
+from django.forms.models import model_to_dict
 
 
 class BeersServiceTest(TestCase):
 
     def setUp(self):
-        self.beer = BeerFactory.create()
+        self.beer = BeerFactory.build()
         self.service = BeerService()
 
     def test_saved_beer(self):
-        res = self.service.add_beer(self.beer)
+        res = self.service.add_beer(model_to_dict(self.beer))
         self.assertEqual(res, True)
 
+
+    def test_retrieved_beer(self):
+        self.beer.save()
+        same_beer = self.service.find_beer_by_id(self.beer.id)
+        self.assertEqual(self.beer, same_beer)
+
+    def test_no_beer_found(self):
+        with pytest.raises(IndexError):
+            self.service.find_beer_by_id(999)
